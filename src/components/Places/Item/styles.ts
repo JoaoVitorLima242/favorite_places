d4f3abe557defecc9ea4a0820d352f0e97b93672
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-13 22:44:17 -0300

    add place item

---
+++ src/components/Places/Item/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.Pressable`
+    
+`
+
+export const Image = styled.Image`
+`
+
+export const Information = styled.View`
+`
+
+export const Title = styled.Text`
+`
+export const Address = styled.Text`
+`
