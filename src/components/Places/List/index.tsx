c87286de954e087df90ae0f2a492386d4fa3c506
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-13 22:51:07 -0300

    starting all places view

--- src/components/Places/List/index.tsx
+++ src/components/Places/List/index.tsx
@@ -1,6 +1,11 @@
 import { FlatList, Text } from "react-native"
+
+// Types
 import Place from "../../../models/Place"
+// Components
 import PlaceItem from "../Item"
+// Styles
+import * as S from './styles'
 
 type Props = {
     places: Place[]
@@ -10,6 +15,14 @@ const PlacesList = ({
     places,
 
 }: Props) => {
+    if (!places || places.length === 0) {
+        return (
+            <S.Fallback>
+                <S.FallbackText>No places added yet - startin adding some! </S.FallbackText>
+            </S.Fallback>
+        )
+    }
+
     return (
         <FlatList 
             data={places}
