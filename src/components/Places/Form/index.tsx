f040e4ea0502cd8b672186959d81aa93219095b8
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-13 22:55:11 -0300

    add places & add react-navigation to package

---
+++ src/components/Places/Form/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native'
+
+import * as S from './styles'
+
+const PlaceForm = () => {
+    return (
+        <S.Wrapper>
+            <Text>Place Form</Text>
+        </S.Wrapper>
+    )
+}
+
+export default PlaceForm
---
+++ src/screens/AddPlace/index.tsx
@@ -0,0 +1,9 @@
+import PlaceForm from "../../components/Places/Form"
+
+const AddPlaceScreen = () => {
+    return (
+        <PlaceForm />
+    )
+}
+
+export default AddPlaceScreen
