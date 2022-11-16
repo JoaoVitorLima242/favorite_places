c4ce494f95791fd57714f3d4a7223a3370dc1b33
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-15 21:19:39 -0300

    picking currency lovation

--- src/components/ui/LocationPicker/index.tsx
+++ src/components/ui/LocationPicker/index.tsx
@@ -1,19 +1,51 @@
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from 'expo-location'
+import { Alert } from 'react-native'
+
 import Button from '../Button'
 import * as S from './styles'
 
 const LocationPicker = () => {
-    const getLocationHandler = () => {
+    const [locationPermissionInformation, requestPermission] = useForegroundPermissions()
 
+    const verifyPermission = async () => {
+        if (locationPermissionInformation?.status === PermissionStatus.UNDETERMINED) {
+            return await requestPermission()
+        }
+
+        if (locationPermissionInformation?.status === PermissionStatus.DENIED) {
+            Alert.alert(
+                'Insufficient Permissions!',
+                'You need to grant location permissions to use this app.'
+            )
+
+            return false
+        }
+
+        return true
+    }
+    
+    const getLocationHandler = async () => {
+        const hasPermission = await verifyPermission()
+        if(!hasPermission) {
+            return
+        }
+
+        const location = await getCurrentPositionAsync()
+
+        console.log(location)
     }
 
-    const pickOnMapHandler = () => {
-
+    const pickOnMapHandler = async () => {
+        const hasPermission = await verifyPermission()
+        if(!hasPermission) {
+            return
+        }
     }
 
     return (
         <S.Wrapper>
             <S.MapPreview>
-                
+
             </S.MapPreview>
             <S.Buttons>
                 <Button icon='location' onPress={getLocationHandler}>Locate User</Button>
