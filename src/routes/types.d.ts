0e50d012e77ad8414bcf5ac7b64e00a6e502c779
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-19 19:02:20 -0300

    saving location

--- src/routes/types.d.ts
+++ src/routes/types.d.ts
@@ -1,8 +1,11 @@
 import { StackScreenProps } from "@react-navigation/stack";
+import { TLocation } from "../helpers/location";
 
 export type RootStackParamList = {
     AllPlaces: undefined;
-    AddPlace: undefined;
+    AddPlace: {
+        pickedLocation?: TLocation
+    };
     Map: undefined;
 };
 
--- src/screens/Map/index.tsx
+++ src/screens/Map/index.tsx
@@ -1,11 +1,19 @@
-import { useState } from "react"
+import { useCallback, useLayoutEffect, useState } from "react"
+import { Alert } from "react-native"
 import { MapPressEvent, Marker, Region } from "react-native-maps"
+import { StackScreenProps } from '@react-navigation/stack';
+
 
 import { TLocation } from "../../helpers/location"
 
 import * as S from './styles'
+import { RootStackParamList } from "../../routes/types";
+import IconButton from "../../components/ui/IconButton";
 
-const Map = () => {
+type Props = StackScreenProps<RootStackParamList, 'Map'>;
+
+
+const Map = ({ navigation }: Props) => {
     const [ selectedLocation, setSelectedLocation ] = useState<TLocation | null>(null)
 
     const region: Region = {
@@ -14,6 +22,34 @@ const Map = () => {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
     }
+
+    const savePickedLocationHandler = useCallback(() => {
+        if(!selectedLocation) {
+            Alert.alert(
+                'No location picked!',
+                'You have to pick a location (by tapping on the map) first!'
+            )
+            return
+        }
+
+        navigation.navigate('AddPlace', {
+            pickedLocation: selectedLocation
+        })
+    }, [navigation, selectedLocation])
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: 
+            ({tintColor}) => (
+                <IconButton 
+                    color={tintColor as string}
+                    icon='save'
+                    size={24}
+                    onPress={savePickedLocationHandler}
+                />
+            )
+        })
+    }, [navigation, savePickedLocationHandler])
 
     const selectLoactionHandler = (event: MapPressEvent) => {
         const { latitude, longitude } = event.nativeEvent.coordinate
