abb12d3c75a6cc46a610e4166b9c6da3c1a83efa
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-20 21:17:57 -0300

    stating tables

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { defaultTheme } from './src/assets/styles/theme';
 import Navigation from './src/routes';
+import { init } from './src/services/database';
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false)
+
+
+
+  useEffect(() => {
+    init().then(() => {
+      setDbInitialized(true)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
+  if (!dbInitialized) return <AppLoading />
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Navigation />
